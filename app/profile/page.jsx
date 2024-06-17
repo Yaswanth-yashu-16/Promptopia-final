8696b242da6276c1b83cffec04868f4be72910d4
Yaswanth <yaswanthvempuluru@gmail.com>
2024-06-17 18:29:01 +0530

    first commit

---
+++ app/profile/page.jsx
@@ -0,0 +1,62 @@
+"use client"
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import Profile from '@components/Profile'
+const MyProfile = () => {
+    const router = useRouter()
+    const {data : session} = useSession()
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await fetch(`/api/users/${session?.user.id}/posts`);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setPosts(data);
+            } catch (error) {
+                console.error('Failed to fetch posts:', error);
+            }
+        };
+        if(session?.user.id)
+            fetchPosts();
+    }, []);
+    const handleDelete = async (post) => {
+
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
+
+        if(hasConfirmed){
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`,{
+                    method : 'DELETE'
+                })
+
+                const filteredPosts = posts.filter((p)=>p._id!==post._id)
+
+                setPosts(filteredPosts)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+    }
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+
+
+
+    return (
+        <Profile
+            name="My"
+            desc="Welcome to personalized profile page"
+            data={posts}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+        />
+    )
+}
+
+export default MyProfile
---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config;
+  }
+}
+
+export default nextConfig;
+
+// images: {
+//   remotePatterns: [
+//       {
+//           protocol: 'https',
+//           hostname: '*.googleusercontent.com',
+//           port: '',
+//           pathname: '/**',
+//       },
+//   ],
+// },
